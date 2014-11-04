6c2b93132318186dae7399a8950187e850e83cba
Lukas Bischofberger <lukasbi@student.ethz.ch>
2014-11-05 00:42:34 +0100

    added ndks

--- nativeTest/obj/local/armeabi-v7a/objs/myjni/native.o.d
+++
@@ -1,5 +0,0 @@
-D:/Documents/GitHub/BA/nativeTest//obj/local/armeabi-v7a/objs/myjni/native.o: \
- D:/Documents/GitHub/BA/nativeTest//jni/native.cpp \
- C:/android/ndk/platforms/android-9/arch-arm/usr/include/jni.h
-
-C:/android/ndk/platforms/android-9/arch-arm/usr/include/jni.h:
